024d11e620381db5144c719016cd2cc78af78d2e
starsnee <starsnee@users.noreply.github.com>
2025-11-08 19:10:27 -0600

    adding pages and api url (frontend)

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import "./global.css"
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 export default function App() {
   return (
@@ -24,12 +14,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ config.ts
@@ -0,0 +1,2 @@
+export const API_BASE_URL = 'https://integrate.api.nvidia.com/v1';
+
